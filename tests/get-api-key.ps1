2de51ddccb682cf6c1171aa02b12b4db2c576187
Matthew Hodgkins <matthodge@gmail.com>
2018-05-02 22:14:23 +0200

    More Build Updates

--- tests/get-api-key.ps1
+++ tests/get-api-key.ps1
@@ -1,10 +1,17 @@
-$OctopusURI = "http://172.25.16.1:81" #Octopus URL
+# Copy Files from Image
+
+$octopusContainerId = & docker ps -aq
+
+Start-Process docker -ArgumentList "cp $($octopusContainerId):`"C:\Program Files\Octopus Deploy\Octopus\Octopus.Client.dll`" ." -Wait -NoNewWindow
+Start-Process docker -ArgumentList "cp $($octopusContainerId):`"C:\Program Files\Octopus Deploy\Octopus\Newtonsoft.Json.dll`" ." -Wait -NoNewWindow
+
+$OctopusURI = "http://localhost:81" #Octopus URL
 
 $APIKeyPurpose = "PowerShell" #Brief text to describe the purpose of your API Key.
 
 #Adding libraries. Make sure to modify these paths acording to your environment setup.
-Add-Type -Path "C:\Program Files\Octopus Deploy\Octopus\Newtonsoft.Json.dll"
-Add-Type -Path "C:\Program Files\Octopus Deploy\Octopus\Octopus.Client.dll"
+Add-Type -Path "Newtonsoft.Json.dll"
+Add-Type -Path "Octopus.Client.dll"
 
 #Creating a connection
 $endpoint = new-object Octopus.Client.OctopusServerEndpoint $OctopusURI
@@ -12,8 +19,8 @@ $repository = new-object Octopus.Client.OctopusRepository $endpoint
 
 #Creating login object
 $LoginObj = New-Object Octopus.Client.Model.LoginCommand
-$LoginObj.Username = 'AppveyorAdmin'
-$LoginObj.Password = 'GH8sf8vgs435fd'
+$LoginObj.Username = $OctopusUsername
+$LoginObj.Password = $OctopusPassword
 
 #Loging in to Octopus
 $repository.Users.SignIn($LoginObj)
@@ -28,4 +35,4 @@ $ApiObj = $repository.Users.CreateApiKey($UserObj, $APIKeyPurpose)
 # CREATE ENVIRONMENT
 #############################
 
-& octo create-environment --name Testing --server $OctopusURI --apikey $ApiObj.ApiKey
+& octo create-environment --name Testing --server http://localhost --apikey $ApiObj.ApiKey
